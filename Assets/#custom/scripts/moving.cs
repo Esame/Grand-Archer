bd406969bd3cd3a773d80f725e020345c131f3ad
Esame <evan@clanmclean.net>
2016-10-28 08:30:50 -0700

    fuckyou

---
+++ Assets/#custom/scripts/moving.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+public float spd = 100;
+public class moving : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+			
+	}
+	void FixedUpdate () {
+		float dir = Input.GetAxis("Horizontal");
+		GetComponent<Rigidbody2D>().AddForce(dir * float * Vector2.right);
+	}
+}
